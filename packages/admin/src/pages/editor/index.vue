e04e4364cd9bd0ecad95315230f9c45e1d000f22
lwdggm <lwdggm@gmail.com>
2017-08-27 18:40:12 +0800

    update

--- packages/admin/src/pages/editor/index.vue
+++ packages/admin/src/pages/editor/index.vue
@@ -1,24 +1,31 @@
 <template>
   <article v-loading.full="loading">
     <el-form label-width="50px">
-      <el-row :gutter="12">
+      <el-row :gutter="12" v-if="isMarkdown()">
         <el-col :md="12">
           <el-input placeholder="请输入文章标题" v-model="title" :size="size"></el-input>
         </el-col>
-        <el-col v-if="isMarkdown()" :xs="24" :sm="14" :md="6">
+        <el-col :xs="24" :sm="14" :md="6">
           <el-input placeholder="存放文件夹, 可以为空" v-model="path" :size="size">
              <template slot="prepend">{{ prefix }}</template>
           </el-input>
         </el-col>
         <el-col :xs="24" :sm="10" :md="6">
-          <el-button v-if="isMarkdown()" type="primary" @click="save" :size="size">保存</el-button>
-          <el-button v-if="isMarkdown()" @click="newPost" icon="plus" :size="size"></el-button>
+          <el-button type="primary" @click="save" :size="size">保存</el-button>
+          <el-button @click="newPost" icon="plus" :size="size"></el-button>
           <el-button type="danger" icon="delete" @click="removeFile" :size="size"></el-button>
         </el-col>
-        <el-col v-if="!isMarkdown()">
+      </el-row>
+      <el-row :gutter="12" v-else>
+        <el-col :span="20">
           <el-input readonly v-model="downloadUrl" ref="copyText" :size="size">
-            <template slot="append"><el-button @click="copy" :size="size">复制文件链接</el-button></template>
+            <template slot="append">
+              <el-button @click="copy" :size="size">复制文件链接</el-button>
+            </template>
           </el-input>
+        </el-col>
+        <el-col :span="4">
+          <el-button type="danger" icon="delete" @click="removeFile" :size="size"></el-button>
         </el-col>
       </el-row>
     </el-form>
@@ -209,7 +216,7 @@ export default {
       this.sha = null;
     },
     removeFile(data) {
-      return this.$confirm('是否要删除此文章？')
+      return this.$confirm('是否要删除此文件？')
       .then(() => {
         this.loading = true;
         return repo.contents(this.$route.query.path)
